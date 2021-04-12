fdb96cba96bbe5ec42adb9af2bfd4f217b4d3b6b
Ian <iankimeli@gmail.com>
2021-04-12 08:06:08 +0300

    update credential

--- credential.py
+++ credential.py
@@ -19,3 +19,55 @@ class Credential:
 
     # Empty list of credentials
     credential_list = []
+
+    def __init__(self, user_password, credential_name, credential_password):
+        '''
+        __init__ method to define the properties of a User object
+
+        Args:
+            user_password : password of the user
+            credential_name : name of an account
+            credential_password : password for the account
+        '''
+        self.user_password = user_password
+        self.credential_name = credential_name
+        self.credential_password = credential_password
+
+    def save_credential(self):
+        '''
+        Method that saves a user's credentials to credential list
+        '''
+        Credential.credential_list.append(self)
+
+    @classmethod
+    def generate_password(cls):
+        '''
+        Method that generates a random alphanumeric password
+        '''
+        # Length of the generated password
+        size = 8
+
+        # Generate random alphanumeric 
+        alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
+
+        # Create password
+        password = ''.join( choice(alphanum) for num in range(size) )
+        
+        return password
+
+    @classmethod
+    def display_credential(cls,password):
+        '''
+        Method that returns the credential list
+
+        Args:
+            password : the user password
+        '''
+        user_credential_list = []
+
+        for credential in cls.credential_list:
+            if credential.user_password == password:
+                user_credential_list.append(credential)
+
+        return user_credential_list
+    
