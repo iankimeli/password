f16887e32380e240e11564083eb55724a14381e2
Ian <iankimeli@gmail.com>
2021-04-12 07:50:45 +0300

    update credential test

--- credential_test.py
+++ credential_test.py
@@ -23,3 +23,28 @@ class TestCredential(unittest.TestCase):
         # Create credential object
         self.new_credential = Credential("doe","Yahoo","yahoo17")
 
+    def tearDown(self):
+        '''
+        tearDown method that cleans up after each test case is run
+        '''
+
+        Credential.credential_list = []
+
+    def test_init(self):
+        '''
+        Test case to test if the object is initialised properly
+        '''
+        self.assertEqual( self.new_credential.user_password, "doe")
+        self.assertEqual( self.new_credential.credential_name, "Yahoo" )
+        self.assertEqual( self.new_credential.credential_password, "yahoo17" )
+
+    def test_save_credential(self):
+        '''
+        Test case to test if the user object is saved into the user list
+        '''
+
+        # Save the new credential
+        self.new_credential.save_credential()
+
+        self.assertEqual( len(Credential.credential_list), 1 )
+
